6d894144d9f9bbf8606bb51260b2c05844c5dae1
Oleg Sobolev <sobolev_ov@mail.ru>
2023-11-18 01:20:52 +0100

    Выводить контекст ошибки при отсутствии start/end

--- src/jkara/ass/Aligned.java
+++ src/jkara/ass/Aligned.java
@@ -1,6 +1,7 @@
 package jkara.ass;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -27,6 +28,16 @@ final class Aligned {
         return list.get(i).ch;
     }
 
+    private static double getDouble(JSONObject cseg, String key, int i, JSONObject seg) {
+        double value = cseg.optDouble(key);
+        if (Double.isNaN(value)) {
+            String text = seg.optString("text");
+            String where = text == null ? "segment " + i : "'" + text + "'";
+            throw new JSONException(String.format("Missing \"%s\" field at %s", key, where));
+        }
+        return value;
+    }
+
     static Aligned read(Path file) throws IOException {
         List<CSegment> buf = new ArrayList<>();
         try (InputStream is = Files.newInputStream(file)) {
@@ -37,8 +48,8 @@ final class Aligned {
                 JSONArray csegs = seg.getJSONArray("char-segments");
                 for (int j = 0; j < csegs.length(); j++) {
                     JSONObject cseg = csegs.getJSONObject(j);
-                    double start = cseg.getDouble("start");
-                    double end = cseg.getDouble("end");
+                    double start = getDouble(cseg, "start", i, seg);
+                    double end = getDouble(cseg, "end", i, seg);
                     char ch = cseg.getString("char").charAt(0);
                     buf.add(new CSegment(ch, Timestamps.create(start, end)));
                 }
