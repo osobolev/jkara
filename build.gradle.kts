a776a84e29300833112bc57ba8cf11dece294bb8
Oleg Sobolev <sobolev_ov@mail.ru>
2024-11-28 04:55:30 +0100

    Dependencies update

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ tasks.withType(JavaCompile::class) {
 
 dependencies {
     implementation("io.github.java-diff-utils:java-diff-utils:4.12")
-    implementation("org.json:json:20231013")
+    implementation("org.json:json:20240303")
 }
 
 configurations["manualImplementation"].extendsFrom(configurations["implementation"])
