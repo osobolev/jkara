ac903a5cfaa3627120cb3fc6bdaa899ec40a67ee
Oleg Sobolev <sobolev_ov@mail.ru>
2025-11-16 14:02:39 +0100

    Revert to standard WhisperX source

--- src/jkara/setup/SoftSources.java
+++ src/jkara/setup/SoftSources.java
@@ -16,7 +16,7 @@ public record SoftSources(
         this(
             "https://www.python.org/ftp/python/3.10.11/python-3.10.11-embed-amd64.zip",
             "https://bootstrap.pypa.io/get-pip.py",
-            "https://github.com/osobolev/whisperX/archive/refs/heads/main.zip",
+            "https://github.com/m-bain/whisperX/archive/refs/heads/main.zip",
             "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip"
         );
     }
